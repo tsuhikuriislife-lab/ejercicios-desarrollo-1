4fe9a910e21dbcce58ac48d816d139503162d55a
Mateo <tsuhikuriislife@gmail.com>
2026-03-11 15:09:00 -0500

    Add new options for sports store and spa services

--- ejercicios.py
+++ ejercicios.py
@@ -1,7 +1,7 @@
 #Empezamos saludando al usuario y mostrandole las opciones.
 
 print("Bienvenido al conjunto de ejercicios!")
-print("Lista de ejercicios:\n1.Heladeria\n2.Gimnasio\n3.Cafeteria\n4.Cine\n5.Tienda de mascotas\n6.Parqueadero\n7.Peluqueria")
+print("Lista de ejercicios:\n1.Heladeria\n2.Gimnasio\n3.Cafeteria\n4.Cine\n5.Tienda de mascotas\n6.Parqueadero\n7.Peluqueria\n8.Tienda deportiva\n9.Spa\n10.Academia de Baile\n11.Heladeria #2\n12.Gimnasio #2\n13.Cafeteria #2\n14.Cine #2\n15.Parqueadero #2\n16.Tienda de mascotas #2\n17.Peluqueria #2\n18.Centro de Idiomas\n19.Tienda de Ropa deportiva\n20.Club recreativo\n21.Salir")
 opcion_inicio = int(input("-- A que ejercicio quieres ir?: "))
 
 if opcion_inicio == 1:
@@ -188,5 +188,117 @@ elif opcion_inicio == 7:
             print("Fuera de horario.")
         opcion_salida = input("Salir? S/N: ")
         if opcion_salida == "s":
+            salir += 1
+
+elif opcion_inicio == 8:
+    print("Bienvenido a la tienda deportiva!")
+    productos = int(input("Por favor digita la cantidad de productos: "))
+    cantidad_productos = 0
+    for i in range(0, productos):
+        precios = int(input("Por favor digita el valor del producto: $"))
+        if precios >= 100000:
+            cantidad_productos += 1
+    print(f"{cantidad_productos} productos con precios superiores a $100000!")
+
+elif opcion_inicio == 9:
+    servicios = ["masaje","facial","manicure"]
+    print("Bienvenido al Spa!")
+    print("Estos son nuestros servicios disponibles:\n1.Masaje\n2.Facial\n3.Manicure")
+    spa = input("Que servicio deseas cotizar?: ")
+    if spa in servicios:
+        print("Tu servicio se encuentra disponible!")
+    else:
+        print("Servicio no disponible.")
+
+elif opcion_inicio == 10:
+    print("Bienvenido a la academia de baile!")
+    asistencias = int(input("Por favor digite cuantas inasistencias tuvo este mes: "))
+    if asistencias < 5:
+        print("Asistencias bajas.")
+    elif asistencias in range (5, 8):
+        print("Asistencias medias.")
+    elif asistencias > 9:
+        print("Asistencias altas.")
+
+elif opcion_inicio == 11:
+    print("Bienvenido a la heladeria!")
+    cono = 3000
+    vaso = 4000
+    banana = 9000
+    print(f"Este es nuestro menu de precios:\n1.Cono: ${cono}\n2.Vaso: ${vaso}\n3.Banana split: ${banana}")
+    cantidad_cono = 0
+    cantidad_vaso = 0
+    cantidad_banana = 0
+    total = 0
+    salir = 0
+    ciclo = 0
+    while salir == 0:
+        cantidad_cono_temporal = 0
+        cantidad_vaso_temporal = 0
+        cantidad_banana_temporal = 0
+        print(f"Pedido #{ciclo+1}")
+        producto = int(input("Que producto desea ordenar?: "))
+        if producto == 1:
+            cantidad = int(input("Cuantos desea ordenar?: "))
+            cantidad_cono_temporal += cantidad
+            cantidad_cono += cantidad
+            total_cono = cono * cantidad_cono_temporal
+            total += total_cono
+            print(f"El total es: ${total_cono}")
+        elif producto == 2:
+            cantidad = int(input("Cuantos desea ordenar?: "))
+            cantidad_vaso_temporal += cantidad
+            cantidad_vaso += cantidad
+            total_vaso = vaso * cantidad_vaso_temporal
+            total += total_vaso
+            print(f"El total es: ${total_vaso}")
+        elif producto == 3:
+            cantidad = int(input("Cuantos desea ordenar?: "))
+            cantidad_banana_temporal += cantidad
+            cantidad_banana += cantidad
+            total_banana = banana * cantidad_banana_temporal
+            total += total_banana
+            print(f"El total es: ${total_banana}")
+        opcion_salida = input("Salir? S/N: ")
+        if opcion_salida == "s":
+            salir += 1
+        ciclo += 1
+
+    print(f"Total del dia:\nConos vendidos: {cantidad_cono}\nVasos vendidos: {cantidad_vaso}\nBanana split vendidas: {cantidad_banana}\nTotal vendido: ${total}\nClientes atendidos: {ciclo}")
+
+elif opcion_inicio == 12:
+    print("Bienvenido al gimnasio!")
+    ciclos = int(input("Cuantas personas vas a registrar?: "))
+    bajo = 0
+    medio = 0
+    alto = 0
+    usuario = 0
+    for i in range (0, ciclos):
+        print(f"Usuario #{usuario+1}")
+#        nombre = input("Ingrese su nombre: ")
+        dias = int(input("Cuantos dias asistio esta semana?: "))
+#        minutos = int(input("Cuantos minutos en promedio duraron sus sesiones?: "))
+        if dias < 3:
+            print("Bajo compromiso.")
+            bajo += 1
+        elif dias in range (3,4):
+            print("Compromiso medio.")
+            medio += 1
+        elif dias >= 5:
+            print("Compromiso alto.")
+            alto += 1
+        usuario += 1
+    print(f"Cantidad de usuarios: {ciclos}\nBajo compromiso: {bajo}\nMedio compromiso: {medio}\nAlto compromiso: {alto}")
+
+elif opcion_inicio == 13:
+    print("Bienvenido a la cafeteria!")
+    cafe = 4000
+    capuchino = 7000
+    pastel = 6000
+    total = 0
+    salir = 0
+    c
+    while salir == 0:
+        print
             salir += 1
         
